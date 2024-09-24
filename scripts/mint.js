337f4e2b4b932cb135911b7cab610315f4c0d41a
Lanky-23 <bala.laankwap@lmu.edu.ng>
2024-09-24 22:48:34 +0100

    Deploying an ERC721 Smart Contract

---
+++ scripts/mint.js
@@ -0,0 +1,27 @@
+// Import necessary modules from Hardhat and SwisstronikJS
+const hre = require("hardhat");
+const { SwisstronikPlugin } = require("@swisstronik/web3-plugin-swisstronik");
+hre.web3.registerPlugin(new SwisstronikPlugin(hre.network.config.url));
+async function main() {
+    const replace_contractAddress = "0x1d05C89A36E2852275fb14B61D1cFea11685caC1";
+    const [from] = await hre.web3.eth.getAccounts();
+    const contractFactory = await hre.ethers.getContractFactory("TestNFT");
+    const ABI = JSON.parse(contractFactory.interface.formatJson());
+    const contract = new hre.web3.eth.Contract(ABI, replace_contractAddress);
+    const replace_functionArgs = "0xC99474e3fB05097cA90C1d1E299a9e69cc517141"; // Recipient address
+    console.log("Minting 1 token...");
+    try {
+        const transaction = await contract.methods.safeMint(replace_functionArgs).send({ from });
+        console.log("Transaction submitted! Transaction details:", transaction);
+        // Display success message with recipient address
+        console.log(`Transaction completed successfully! ✅ Non-Fungible Token minted to ${replace_functionArgs}`);
+        console.log("Transaction hash:", transaction.transactionHash);
+    } catch (error) {
+        console.error(`Transaction failed! Could not mint NFT.`);
+        console.error(error);
+    }
+}
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
