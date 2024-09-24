0f55a5c7f534b4cc9b93e9d7ba26e77c81b7b576
Lanky <bala.laankwap@lmu.edu.ng>
2024-09-24 22:50:56 +0100

    Update mint.js

--- scripts/mint.js
+++ scripts/mint.js
@@ -3,12 +3,12 @@ const hre = require("hardhat");
 const { SwisstronikPlugin } = require("@swisstronik/web3-plugin-swisstronik");
 hre.web3.registerPlugin(new SwisstronikPlugin(hre.network.config.url));
 async function main() {
-    const replace_contractAddress = "0x1d05C89A36E2852275fb14B61D1cFea11685caC1";
+    const replace_contractAddress = "<Replace_Contract_Address>";
     const [from] = await hre.web3.eth.getAccounts();
     const contractFactory = await hre.ethers.getContractFactory("TestNFT");
     const ABI = JSON.parse(contractFactory.interface.formatJson());
     const contract = new hre.web3.eth.Contract(ABI, replace_contractAddress);
-    const replace_functionArgs = "0xC99474e3fB05097cA90C1d1E299a9e69cc517141"; // Recipient address
+    const replace_functionArgs = "<Replace_Wallet_Address>"; // Recipient address
     console.log("Minting 1 token...");
     try {
         const transaction = await contract.methods.safeMint(replace_functionArgs).send({ from });
